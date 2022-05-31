18315b0f1d23372fa59c5b5b340f58dfa40916c3
kacpermejs <kacpermejs@gmail.com>
2022-05-31 21:54:13 +0200

    próba zrobienia kopca

--- SPDSchrage.cpp
+++ SPDSchrage.cpp
@@ -15,15 +15,15 @@
 
 
 template <typename T>
-class Heap
+class MaxHeap
 {
-    std::vector<T> data;
+    T* data;
     int capacity;
     int num_of_elem;
 public:
-    Heap();
+    MaxHeap();
     void resize();
-    void insert(const T& newElement);
+    void push(const T& newElement);
     void removeMax();
     std::size_t size();
     bool empty();
@@ -31,30 +31,37 @@ public:
 };
 
 template <typename T>
-Heap<T>::Heap()
+MaxHeap<T>::MaxHeap()
 {
-    data.resize(DEFAULT_HEAP_SIZE+1);
+    //data.resize(DEFAULT_HEAP_SIZE+1);
+    data = new T[DEFAULT_HEAP_SIZE + 1];
     capacity = DEFAULT_HEAP_SIZE;
     num_of_elem = 0;
 }
 
 template <typename T>
-void Heap<T>::resize()
+void MaxHeap<T>::resize()
 {
     int old_size = capacity;
     capacity *= 2;
-    std::vector<T> temp_data;
-    temp_data.resize(capacity);
+    T* temp_data;
+    //temp_data.resize(capacity);
+    temp_data = new T[capacity];
     for (int i = 1; i < old_size; i++)
     {
         temp_data[i] = data[i];
     }
-
-    data = temp_data;
+    //delete [] data;
+    data = new T[capacity];
+    for (int i = 1; i < old_size; i++)
+    {
+        data[i] = temp_data[i];
+    }
+    
 }
 
 template <typename T>
-void Heap<T>::insert(const T& newElement)
+void MaxHeap<T>::push(const T& newElement)
 {
     if (capacity - 1 == size())
         resize();
@@ -76,7 +83,165 @@ void Heap<T>::insert(const T& newElement)
 }
 
 template <typename T>
-void Heap<T>::removeMax()
+void MaxHeap<T>::removeMax()
+{
+    if (!empty())
+    {
+        int id = size();
+        T temp = data[id];
+        data[id] = data[1];
+        data[1] = temp;
+        num_of_elem--;
+
+        id = 1;
+
+        while (id * 2 < size()) //has left child
+        {
+
+            if (data[id * 2] < data[id])//if left child bigger
+            {
+                if ((id * 2) + 1 < size())//if has a right child
+                {
+                    if (data[id * 2] < data[(id * 2) + 1])
+                    {
+                        //swap parent - left child
+                        T temp = data[id];
+                        data[id] = data[id * 2];
+                        id = id * 2;
+                        data[id] = temp;
+                    }
+                    else
+                    {
+                        //swap parent - right child
+                        T temp = data[id];
+                        data[id] = data[(id * 2) + 1];
+                        id = (id * 2) + 1;
+                        data[id] = temp;
+                    }
+                }
+                else
+                {
+                    //swap parent - right child
+                    T temp = data[id];
+                    data[id] = data[(id * 2) + 1];
+                    id = (id * 2) + 1;
+                    data[id] = temp;
+                }
+            }
+            else if ((id * 2) + 1 < size())//if has a right child
+            {
+                if (data[(id * 2) + 1] < data[id])
+                {
+                    //swap parent - right child
+                    T temp = data[id];
+                    data[id] = data[(id * 2) + 1];
+                    id = (id * 2) + 1;
+                    data[id] = temp;
+                }
+
+            }
+            else
+                break;
+        }
+    }
+    else
+        throw HEAP_EMPTY_EXCEPTION;
+
+
+}
+
+template <typename T>
+std::size_t MaxHeap<T>::size()
+{
+    return num_of_elem;
+}
+
+template <typename T>
+bool MaxHeap<T>::empty()
+{
+    return num_of_elem == 0;
+}
+
+template <typename T>
+T& MaxHeap<T>::operator[](int index)
+{
+    static T t;
+
+    t = data[index];
+
+    return t;
+}
+
+//===========================================================================
+template <typename T>
+class MinHeap
+{
+    T* data;
+    int capacity;
+    int num_of_elem;
+public:
+    MinHeap();
+    void resize();
+    void push(const T& newElement);
+    void removeMax();
+    std::size_t size();
+    bool empty();
+    T& operator[](int index);
+};
+
+template <typename T>
+MinHeap<T>::MinHeap()
+{
+    //data.resize(DEFAULT_HEAP_SIZE+1);
+    data = new T[DEFAULT_HEAP_SIZE + 1];
+        capacity = DEFAULT_HEAP_SIZE;
+    num_of_elem = 0;
+}
+
+template <typename T>
+void MinHeap<T>::resize()
+{
+    int old_size = capacity;
+    capacity *= 2;
+    T* temp_data;
+    //temp_data.resize(capacity);
+    temp_data = new T[capacity];
+    for (int i = 1; i < old_size; i++)
+    {
+        temp_data[i] = data[i];
+    }
+    //delete [] data;
+    data = new T[capacity];
+    for (int i = 1; i < old_size; i++)
+    {
+        data[i] = temp_data[i];
+    }
+}
+
+template <typename T>
+void MinHeap<T>::push(const T& newElement)
+{
+    if (capacity - 1 == size())
+        resize();
+
+
+    int id = size() + 1;
+    data[id] = newElement;
+
+    while (data[id] > data[id / 2] && (id / 2) != 0)
+    {
+        //swap child - parent
+        T temp = data[id / 2];
+        data[id / 2] = data[id];
+        data[id] = temp;
+        id = id / 2;
+    }
+    num_of_elem++;
+
+}
+
+template <typename T>
+void MinHeap<T>::removeMax()
 {
     if (!empty())
     {
@@ -144,19 +309,19 @@ void Heap<T>::removeMax()
 }
 
 template <typename T>
-std::size_t Heap<T>::size()
+std::size_t MinHeap<T>::size()
 {
     return num_of_elem;
 }
 
 template <typename T>
-bool Heap<T>::empty()
+bool MinHeap<T>::empty()
 {
     return num_of_elem == 0;
 }
 
 template <typename T>
-T& Heap<T>::operator[](int index)
+T& MinHeap<T>::operator[](int index)
 {
     static T t;
 
@@ -164,6 +329,7 @@ T& Heap<T>::operator[](int index)
 
     return t;
 }
+//============================================================
 
 struct Task
 {
@@ -175,9 +341,13 @@ struct Task
 
     bool finished;
 
+    Task() : R(-1), P(-1), Q(-1), C(-1) {}
     Task(int r, int p, int q) : R(r), P(p), Q(q), C(-1) {}
     Task(int r, int p, int q, int id) : R(r), P(p), Q(q), index(id), C(-1) {}
     Task(const Task& t, int c) : R(t.R), P(t.P), Q(t.Q), index(t.index), C(c) {}
+
+    friend bool operator< (const Task& lhs, const Task& rhs) { return lhs.R < rhs.R; }
+    friend bool operator> (const Task& lhs, const Task& rhs) { return lhs.Q > rhs.Q; }
 
 
 };
@@ -211,11 +381,12 @@ struct compareQ
     }
 };
 
-void Schrage1(std::priority_queue<Task, std::vector<Task>, compareR > &MinHeapR,
+int Schrage1(std::priority_queue<Task, std::vector<Task>, compareR > &MinHeapR,
               std::priority_queue<Task, std::vector<Task>, compareQ > &MaxHeapQ,
               std::vector<Task> &C )
 {
     int t = 0;
+    int Cmax = 0;
     t += MinHeapR.top().R; //czas rozpoczêcia pierwszego zadania
 
     while (!MinHeapR.empty() || !MaxHeapQ.empty())
@@ -231,6 +402,7 @@ void Schrage1(std::priority_queue<Task, std::vector<Task>, compareR > &MinHeapR,
             t += MaxHeapQ.top().P; //dodaj czas trwania zadania dostêpnego
 
             C.push_back(Task(MaxHeapQ.top(), t + MaxHeapQ.top().Q));//doadaj do uszeregowania zadanie i jego czas zakoñczenia
+            Cmax = std::max(Cmax, t + MaxHeapQ.top().Q);
             MaxHeapQ.pop();
         }
         else
@@ -238,71 +410,11 @@ void Schrage1(std::priority_queue<Task, std::vector<Task>, compareR > &MinHeapR,
             t = MinHeapR.top().R;//je¿eli nie ma zadañ dostêpnych, idŸ do momentu w którym pojawi siê zadanie
         }
     }
-}
-
-void Schrage2(std::priority_queue<Task, std::vector<Task>, compareR >& MinHeapR,
-    std::priority_queue<Task, std::vector<Task>, compareQ >& MaxHeapQ,
-    std::vector<Task>& C)
-{
-    int tOld = 0;
-    int t = MinHeapR.top().R; //czas rozpoczêcia pierwszego zadania
-
-    while (!MinHeapR.empty() || !MaxHeapQ.empty())
-    {
-        //dodawanie nowych zadañ ===================================================
-        
-        //dodaj zadania które sta³y siê juz dostêpne
-        while (!MinHeapR.empty() && MinHeapR.top().R <= t)
-        {
-            MaxHeapQ.push(MinHeapR.top());
-            MinHeapR.pop();
-        }
-
-        //dodaj pozosta³¹ czêœæ bierz¹cego zadanoia je¿eli jeszcze zota³a
-        if (!C.empty()) //by³o ju¿ jakieœ zadanie
-        {
-            if (!MaxHeapQ.empty() && MaxHeapQ.top().Q > C.back().Q)
-            {
-                
-                if (t - tOld < C.back().P) // czas od pocz¹tku zadania do jego przerwania < od jego czasu trwania
-                {
-                    Task temp = Task(C.back());
-                    C.back().C = -1;
-                    temp.R = 0;
-                    temp.P -= t - tOld;
-                    MaxHeapQ.push(temp);
-                } 
-            }      
-        }
-        //==========================================================================
-
-
-
-        //ustwianie zadañ w kolejnoœci =============================================
-        if (!MaxHeapQ.empty())
-        {
-            tOld = t;
-            if (!MinHeapR.empty())
-                t = std::min(t + MaxHeapQ.top().P, MinHeapR.top().R); //dodaj czas trwania zadania dostêpnego lub moment w którym mo¿e zostac przewane
-            else
-                t += MaxHeapQ.top().P; //je¿eli nie ma ju¿ zadañ niedostêpnych ustaw czas na koniec bierz¹cego
-            
-            
-
-            C.push_back(Task(MaxHeapQ.top(), t + MaxHeapQ.top().Q));//doadaj do uszeregowania zadanie i jego czas zakoñczenia
-            MaxHeapQ.pop();
-        }
-        else
-        {
-            t = MinHeapR.top().R;//je¿eli nie ma zadañ dostêpnych, idŸ do momentu w którym pojawi siê zadanie
-        }
-        //==========================================================================
-    }
+    return Cmax;
 }
 
 
-
-int Schrage3(std::priority_queue<Task, std::vector<Task>, compareR >& MinHeapR,
+int Schrage2(std::priority_queue<Task, std::vector<Task>, compareR >& MinHeapR,
     std::priority_queue<Task, std::vector<Task>, compareQ >& MaxHeapQ,
     std::vector<Task>& C)
 {
@@ -346,6 +458,38 @@ int Schrage3(std::priority_queue<Task, std::vector<Task>, compareR >& MinHeapR,
     return Cmax;
 }
 
+int Schrage3(MinHeap<Task>& MinHeapR,
+             MaxHeap<Task>& MaxHeapQ,
+             std::vector<Task>& C)
+{
+    int t = 0;
+    int Cmax = 0;
+    t += MinHeapR[1].R; //czas rozpoczêcia pierwszego zadania
+
+    while (!MinHeapR.empty() || !MaxHeapQ.empty())
+    {
+        while (!MinHeapR.empty() && MinHeapR[1].R <= t)
+        {
+            MaxHeapQ.push(MinHeapR[1]);
+            MinHeapR.removeMax();
+        }
+
+        if (!MaxHeapQ.empty())
+        {
+            t += MaxHeapQ[1].P; //dodaj czas trwania zadania dostêpnego
+
+            C.push_back(Task(MaxHeapQ[1], t + MaxHeapQ[1].Q));//doadaj do uszeregowania zadanie i jego czas zakoñczenia
+            Cmax = std::max(Cmax, t + MaxHeapQ[1].Q);
+            MaxHeapQ.removeMax();
+        }
+        else
+        {
+            t = MinHeapR[1].R;//je¿eli nie ma zadañ dostêpnych, idŸ do momentu w którym pojawi siê zadanie
+        }
+    }
+    return Cmax;
+}
+
 int main()
 {
     int N;
@@ -356,13 +500,13 @@ int main()
 
 	int totalC = 0;
 
-    Heap<int> h1;
+    
 
 	std::ifstream data("C:/Users/kacpe/source/repos/SPDSchrage/schr.data.txt");
 
 	std::string tmp;
 
-    std::string dataSource = "data.008:";
+    std::string dataSource = "data.001:";
 	
 	while (tmp != dataSource)
 		data >> tmp;
@@ -371,6 +515,8 @@ int main()
     
     std::priority_queue<Task, std::vector<Task>, compareR > MinHeapR; //niedostêpne
     std::priority_queue<Task, std::vector<Task>, compareQ > MaxHeapQ; //dostêpne
+    //MinHeap<Task> MinHeapR;
+    //MaxHeap<Task> MaxHeapQ;
     //! numerowanie od 1
 	for (int i = 1; i < N+1; i++)
 	{
@@ -381,9 +527,15 @@ int main()
         MinHeapR.push(Task(r, p, q, i));
         //std::cout << "id: " << tasks[i-1].index << " R: " << tasks[i-1].R << " P: " << tasks[i - 1].P << " Q: " << tasks[i - 1].Q << std::endl;
 	}
-    int Cmax = Schrage3(MinHeapR, MaxHeapQ, C);
 
-    std::cout << "Cmax = " << Cmax << std::endl;
+    /*for (int i = 1; i < N+1; i++)
+    {
+        std::cout << MinHeapR[1].index << " ";
+        MinHeapR.pop();
+    }*/
+    int Cmax = Schrage2(MinHeapR, MaxHeapQ, C);
+
+    std::cout << "\nCmax = " << Cmax << std::endl;
     /*
     
     for (int i = 0; i < C.size(); i++)
